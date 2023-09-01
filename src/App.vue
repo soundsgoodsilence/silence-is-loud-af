24566c109967ad73fa2973d5dd6d31f204e1b6a0
Slience <silence@Sliences-Mac-Studio.local>
2023-09-01 09:46:32 +0100

    remove unnecessary files, now image centered with large green border

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,78 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { createApp } from 'vue';
+import App from "./App.vue";  // your App component
+import { VueWindowSizePlugin } from 'vue-window-size/plugin';
+const app = createApp(App);
+app.use(VueWindowSizePlugin);
 </script>
 
+
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main> -->
-  <!-- <main> -->
-    <!-- <center><img alt="Vue logo" class="logo" src="./assets/silence_sounds_question_box_blank_pink.png" width="1500" height="1500" /></center> -->
-    <!-- <img alt="Vue logo" src="./assets/silence_sounds_box_question_mark_black_pink.png" width="2500" height="2500" /> -->
-  <main> 
-    <div class = "wrapper"> 
-      <img alt="Vue logo" src="./assets/trace.svg" width="2500" height="2500" />
-    </div>
-  </main>
-    <!-- <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div> -->
-  <!-- </main> -->
+  <div class="center">
+    <img alt="question_box" src="./assets/silence_sounds_question_box_blank_pink.png" />
+  </div>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
+
+
+
+.center {
+    display: flex;
+    max-width: 200%;
+    max-height: 200%;
+    justify-content: center;
+    align-items: center;
+    border: 555px solid green;
+  } 
+
+  .center img {
+    /* width: 100%; */
+  }
+
+  .zoom-img {
+  width: 500px;
+  height: 500px;
+  overflow: scroll;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.zoom-img img {
+  width: 100%;
+  transition: width 0.25s ease;
 }
 
-@media (min-width: 1024px) {
-  header {
+.zoom-img img:hover {
+  width: 3000px;
+}
+  </style>
+
+
+
+
+<!-- --------- IGNORE --------- -->
+/* @media (min-width: 1024px) {
+  /* header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
+  } */
 
-  .logo {
+  /* .logo {
     margin: 0 2rem 0 0;
-  }
+  } */
 
-  header .wrapper {
+   /* .center {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+    justify-content: center;
+    align-items: center;
+    border: 15px solid green;
+    padding: 0px;
+    margin-right: auto; /* 1 */
+  /* margin-left:  auto;
+  margin: 0px;
+width:100%;
+height:100%;  */ */
+    /* place-items: flex-start; */
+    /* flex-wrap: wrap; */
+  /* }
+} */
+<!-- </style> */ */ -->
