19937137338e6e5a98ca08524495f8126f8765f9
Silence <soundsgoodsilence@gmail.com>
2024-02-17 12:34:32 -0500

    :construction: remove transformESBuild function from vite config

--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,7 @@ export default defineConfig({
    plugins: [
     vue(),
     ts(),
-         // Use the exposed transform from vite, instead of directly
-         // transforming with esbuild
-     return transformWithEsbuild(code, id, {
-          //  loader: 'jsx',
-           loader: { '.js': 'jsx' }, 
-           jsx: 'automatic',
-         })
-   ],
+  ],
    resolve: {
      alias: {
        '@': fileURLToPath(new URL('./src', import.meta.url))
